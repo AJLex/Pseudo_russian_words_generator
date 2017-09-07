280c2120ae7f3b05efbbe4ab98b49e83f71e2757
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-07 12:36:06 +0300

    current_letter -> current_letter_number

--- syllable.py
+++ syllable.py
@@ -1,45 +1,46 @@
-def write_syllables(letter_number, current_letter, word, word_by_syllable, second_condition=False):
+def write_syllables(letter_number, current_letter_number, word, word_by_syllable,
+                    second_condition=False):
     syllable = ''
     if second_condition == False:
-        for letter in range(current_letter, letter_number + 1):
+        for letter in range(current_letter_number, letter_number + 1):
             syllable = syllable + word_list[letter]
     else:
-        for letter in range(current_letter, letter_number + 2):
+        for letter in range(current_letter_number, letter_number + 2):
             syllable = syllable + word_list[letter]
-    word= word + syllable
-    current_letter = len(word)
+    word = word + syllable
+    current_letter_number = len(word)
     word_by_syllable.append(syllable)
-    return current_letter, word, word_by_syllable
+    return current_letter_number, word, word_by_syllable
 
 
 if __name__ == '__main__':
     word_list = list(input('> '))
-    current_letter = 0
+    current_letter_number = 0
     word = ''
     word_by_syllable = []
     vowel = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-    for letter_number in range(current_letter, len(word_list) - 2):
+    for letter_number in range(0, len(word_list)):
         print('letter_number ', letter_number)
         if word_list[letter_number] in vowel and word_list[letter_number + 1] in vowel:
-            current_letter, word, word_by_syllable = write_syllables(letter_number,
-                                                                     current_letter,
+            current_letter_number, word, word_by_syllable = write_syllables(letter_number,
+                                                                     current_letter_number,
                                                                      word,
                                                                      word_by_syllable)
 
         elif word_list[letter_number] in vowel and word_list[letter_number + 1] \
              not in vowel and word_list[letter_number + 2] in vowel:
-            current_letter, word, word_by_syllable = write_syllables(letter_number,
-                                                                     current_letter,
+            current_letter_number, word, word_by_syllable = write_syllables(letter_number,
+                                                                     current_letter_number,
                                                                      word,
                                                                      word_by_syllable)
 
         elif word_list[letter_number] in vowel and word_list[letter_number + 1] \
              not in vowel and word_list[letter_number + 2] not in vowel:
-            current_letter, word, word_by_syllable = write_syllables(letter_number,
-                                                                     current_letter,
+            current_letter_number, word, word_by_syllable = write_syllables(letter_number,
+                                                                     current_letter_number,
                                                                      word,
                                                                      word_by_syllable,
                                                                      second_condition=True) # first condition: v|v or v|cv, second condition: vc|c...cv
                                                                                             # where v - vowel, c - consonant
-            
+
     print(word_by_syllable)
