63f455708d7228a94f215f9a217328b1fbf12855
Aleksei Shuliatev <ashuliatyev@gmail.com>
2017-09-08 16:07:10 +0300

    parameters of syllable writting to file

--- syllable.py
+++ syllable.py
@@ -1,10 +1,11 @@
 import csv
+from collections import Counter
 
 '''
-letter_number - current letter in cicle "for" from __main__
-current_letter_number - is len of variable "word"
+letter_number - current letter in cicle "for"
+current_letter_number - len of variable "word"
 word - sum of syllables
-word_by_syllable - list of syllable
+word_by_syllable_temp - list of syllable
 word_by_letters - incoming word divided by letters
 
 first condition: v|v,
@@ -30,70 +31,104 @@ def obtaining_syllable(current_letter_number, letter_number,
 
 
 def recording_of_syllable(current_letter_number, letter_number, word_by_letters,
-                          word, word_by_syllable, third_condition=False):
+                          word, word_by_syllable_temp, third_condition=False):
     syllable = obtaining_syllable(current_letter_number, letter_number,
                                   word_by_letters, third_condition)
     word += syllable
     current_letter_number = len(word)
-    word_by_syllable.append(syllable)
-    return current_letter_number, letter_number, word_by_syllable, word
+    word_by_syllable_temp.append(syllable)
+    return current_letter_number, letter_number, word_by_syllable_temp, word
 
 
-def last_letters_handler(word_by_letters, word_by_syllable, vowel):
-    if word_by_letters[-3] not in vowel:
+def last_letters_handler(word_by_letters, word_by_syllable_temp, vowel):
+    sign = ['ь', 'ъ']
+    if word_by_letters[-3] not in vowel and\
+       word_by_letters[-3] not in sign:
         if len(word_by_letters) <= 3:
-            word_by_syllable = ['']
-            word_by_syllable[-1] += word_by_letters[-3]
+            word_by_syllable_temp = ['']
+            word_by_syllable_temp[-1] += word_by_letters[-3]
         else:
-            word_by_syllable[-1] += word_by_letters[-3]
-    return word_by_syllable
+            word_by_syllable_temp[-1] += word_by_letters[-3]
+    return word_by_syllable_temp
 
 
-def get_word_by_syllable(word_incoming, current_letter_number=0,
-                         word_by_syllable=[], word=''):
+def get_word_by_syllable(word_incoming,word_by_syllable_temp=[],
+                         current_letter_number=0, word=''):
     word_by_letters = list(word_incoming) + ['а', 'б']
-    vowel = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
-    for letter_number in range(0, len(word_by_letters) - 2):
-        if word_by_letters[letter_number] in vowel and \
-           word_by_letters[letter_number + 1] in vowel:
-            current_letter_number, letter_number , word_by_syllable, word = \
-            recording_of_syllable(current_letter_number, letter_number, word_by_letters,
-                                  word, word_by_syllable)
+    if '-' in word_by_letters:
+        return None
+    else:
+        vowel = ['а', 'у', 'о', 'ы', 'и', 'э', 'я', 'ю', 'ё', 'е']
+        for letter_number in range(0, len(word_by_letters) - 2):
+            if word_by_letters[letter_number] in vowel and \
+               word_by_letters[letter_number + 1] in vowel:
+                current_letter_number, letter_number , word_by_syllable_temp, word = \
+                recording_of_syllable(current_letter_number, letter_number, word_by_letters,
+                                      word, word_by_syllable_temp)
 
-        elif  word_by_letters[letter_number] in vowel and \
-              word_by_letters[letter_number + 1] not in vowel and \
-              word_by_letters[letter_number + 2] in vowel:
-            current_letter_number, letter_number , word_by_syllable, word = \
-            recording_of_syllable(current_letter_number, letter_number, word_by_letters,
-                                  word, word_by_syllable)
+            elif  word_by_letters[letter_number] in vowel and \
+                  word_by_letters[letter_number + 1] not in vowel and \
+                  word_by_letters[letter_number + 2] in vowel:
+                current_letter_number, letter_number , word_by_syllable_temp, word = \
+                recording_of_syllable(current_letter_number, letter_number, word_by_letters,
+                                      word, word_by_syllable_temp)
 
-        elif word_by_letters[letter_number] in vowel and \
-             word_by_letters[letter_number + 1] not in vowel and \
-             word_by_letters[letter_number + 2] not in vowel:
-            current_letter_number, letter_number , word_by_syllable, word = \
-            recording_of_syllable(current_letter_number, letter_number, word_by_letters,
-                                  word, word_by_syllable, third_condition=True)
-    word_by_syllable = last_letters_handler(word_by_letters, word_by_syllable, vowel)
-    return word_by_syllable
+            elif word_by_letters[letter_number] in vowel and \
+                 word_by_letters[letter_number + 1] not in vowel and \
+                 word_by_letters[letter_number + 2] not in vowel:
+                current_letter_number, letter_number , word_by_syllable_temp, word = \
+                recording_of_syllable(current_letter_number, letter_number, word_by_letters,
+                                      word, word_by_syllable_temp, third_condition=True)
+        word_by_syllable_temp = last_letters_handler(word_by_letters, word_by_syllable_temp, vowel)
+        return word_by_syllable_temp
 
 
-def obtain_syllable_parameters(word_by_syllable):
+def obtain_syllable_parameters(word_by_syllable_temp):
     syllable_param = []
-    for syllable in word_by_syllable:
-        previous_syllable = word_by_syllable[word_by_syllable.index(syllable) - 1] \
-        if word_by_syllable.index(syllable) > 0 else ['BEG']
-        syllable_param.append([syllable, previous_syllable,
-                               word_by_syllable.index(syllable),
-                               len(word_by_syllable)])
-    return syllable_param
+    if word_by_syllable_temp:
+        for syllable in word_by_syllable_temp:
+            previous_syllable = word_by_syllable_temp[word_by_syllable_temp.index(syllable) - 1] \
+            if word_by_syllable_temp.index(syllable) > 0 else ['BEG']
+            syllable_param.append((syllable, previous_syllable,
+                                   word_by_syllable_temp.index(syllable),
+                                   len(word_by_syllable_temp)))
+        word_by_syllable_temp = ''
+        return syllable_param
 
 
-# def write_data_of_syllable(fileread='freqrnc2011.csv', filewrite):
-#     with open(fileread, 'r', encoding='utf-8') as f:
-#         fields = ['word', 'PoS', 'Freq', 'R', 'D', 'Doc']
-#         reader = csv.DictReader(f, fields, delimiter='	')
-#         for row in reader:
-#             word_by_syllable = get_word_by_syllable(row.get('word'))
+def write_syllable_parametrs(file_read='freqrnc2011.csv', file_write='temp.txt'):
+    with open(file_read, 'r', encoding='utf-8') as f:
+        sylllable_list = []
+        fields = ['word', 'PoS', 'Freq', 'R', 'D', 'Doc']
+        reader = csv.DictReader(f, fields, delimiter='	')
+        for row in reader:
+            try:
+                syllable_param = []
+                if list(row.get('word'))[0] != list(row.get('word'))[0].upper():
+                    word_by_syllable = get_word_by_syllable(row.get('word'), [])
+                    syllable_param = obtain_syllable_parameters(word_by_syllable)
+                    if syllable_param:
+                        for element in syllable_param:
+                            sylllable_list += element
+                            print(sylllable_list)
+                    # print(syllable_param)
+            except IndexError:
+                print('Нет гласных')
+    with open(file_write, 'w', encoding='utf-8') as f:
+        for element in sylllable_list:
+            f.write(str(element) + '\n')
+
+
+# def obtain_index_of_syllable_param(file_read)
+    # counted_syllable_param = Counter(sylllable_list)
+    # with open(filewrite, 'w', encoding='utf-8') as f:
+    #     fields = ['syllable_param', 'Freq']
+    #     writer = csv.DictWriter(f, fields, delimiter=';')
+    #     writer.writeheader()
+    #     for element in counted_syllable_param.keys():
+    #         print('element ', element)
+    #         writer.writerow(element, counted_syllable_param[element])
 
 
 if __name__ == '__main__':
+    write_syllable_parametrs()
